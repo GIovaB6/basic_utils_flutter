c0cabebeda2cf152a4e6185b962126a015056a37
Giovanni Bruno <giovanni.bruno@cyberneid.com>
2023-11-06 14:30:10 +0100

    add constants.dart

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  // static GlobalKey<CustomBottomBarState> customBottomBarKey = GlobalKey<CustomBottomBarState>();
+
+}
